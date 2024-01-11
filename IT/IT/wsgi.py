2275ce8abd88af6969265cb231eb76765cda9a55
ashash <ash5400@outlook.com>
2024-01-10 22:49:00 -0600

    config added

--- IT/IT/wsgi.py
+++ IT/IT/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+settings_module='IT.deployment' if 'WEBSITE_HOSTNAME'  in os.environ else 'IT.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
